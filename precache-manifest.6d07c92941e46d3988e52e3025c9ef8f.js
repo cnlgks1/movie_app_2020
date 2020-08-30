4abd8b4328100853c0b32ba6be100ab4b69c9ad7
cnlgks1 <you@example.com>
2020-08-30 17:37:26 +0900

    Updates

---
+++ precache-manifest.6d07c92941e46d3988e52e3025c9ef8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27485e07859e59137775c2c2e20a0d1",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "21a5d73b323b207cfc2c",
+    "url": "/movie_app_2020/static/css/main.251cd261.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a5d73b323b207cfc2c",
+    "url": "/movie_app_2020/static/js/main.a5130f86.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
